8b5fec1f440cb08d8af9285c2e832a8dd361d66d
italorr <italorr@MATRIZ-48900244.hapvida.com.br>
2014-10-09 17:20:47 -0300

    Commit inicial

---
+++ src/temp/Parametros.java
@@ -0,0 +1,18 @@
+
+package temp;
+
+public class Parametros {
+    public Parametros(){
+        qtdComandas = 0;
+    }
+    private int qtdComandas;
+
+    public int getQtdComandas() {
+        return qtdComandas;
+    }
+
+    public void setQtdComandas(int qtdComandas) {
+        this.qtdComandas = qtdComandas;
+    }
+    
+}
